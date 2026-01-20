703ef1a19cd0e56df08133a92c8c81bb1b4488d8
Nikitha Praveen <144452300+PES2UG23CS389@users.noreply.github.com>
2026-01-20 16:43:45 +0530

    Add files via upload

---
+++ checkout/__init__.py
@@ -0,0 +1,15 @@
+from database import get_db
+
+def checkout_logic():
+    db = get_db()
+    db.row_factory = None  
+
+    events = db.execute("SELECT fee FROM events").fetchall()
+
+    # 1 / 0   # keep this commented
+
+    total = 0
+    for e in events:
+        total += e[0]
+
+    return total
